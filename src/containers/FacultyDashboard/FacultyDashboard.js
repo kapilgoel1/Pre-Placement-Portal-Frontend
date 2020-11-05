1c8bcf6bac9e37d02e6e6567a4a0e767383d664d
Neha Goel <nehagoel66@gmail.com>
2020-11-05 16:23:32 +0530

    Faculty dashboard

--- src/containers/FacultyDashboard/FacultyDashboard.js
+++ src/containers/FacultyDashboard/FacultyDashboard.js
@@ -2,18 +2,8 @@ import React from 'react';
 import './FacultyDashboard.css';
 import {
     Row, 
-    Col, 
-    Card, 
-    CardImg,
-    CardBody
+    Col
 } from 'reactstrap'; 
-
-import Image1 from '../../assets/img.jpg';
-import Image2 from '../../assets/imgproject.jpg';
-import Image3 from '../../assets/imgproject2.jpg';
-import Image4 from '../../assets/imgproject3.jpg';
-import Image5 from '../../assets/imgproject4.jpg';
-import Image6 from '../../assets/imgproject5.jpg';
 
 import FacultyNavBar from '../../components/FacultyNavBar/FacultyNavBar';
 import ModalExample from '../../components/AddNewResModal/ModelExample';
@@ -32,84 +22,55 @@ const FacultyDashboard = (props) => {
 
     return (
         <div>
-        <FacultyNavBar/>
+            <FacultyNavBar/>
             <div className="container">
-            <Row>
-                <Col md={3}>
-                    <div> 
-                    <Card>
-                        <CardImg top width="100%" src={Image1} alt="Card image cap" />
-                        <CardBody>
-                            <ModalExample buttonLabel="ADD NEW RESOURCES"></ModalExample>
-                        </CardBody>
-                    </Card>
-                    <Card>
-                        <CardImg top width="100%" src={Image4} alt="Card image cap" />
-                        <CardBody>
-                        <button className="functionButtons" onClick={onViewProfile}>VIEW STUDENT PROFILE</button> 
-                        </CardBody>
-                    </Card>
-                    
-                    </div>
-                </Col>
-
-                <Col md={3}>
-                    <div> 
-                    <Card>
-                        <CardImg top width="100%" src={Image2} alt="Card image cap" />
-                        <CardBody>
-                        <AddNewJobModal buttonLabel="ADD NEW JOB POSTINGS"><i class="fas fa-box-open"></i></AddNewJobModal>
-                        </CardBody>
-                    </Card>
-                    <Card>
-                        <CardImg top width="100%" src={Image6} alt="Card image cap" />
-                        <CardBody>
-                        <AddNewTestModal buttonLabel="ADD NEW TEST"/>
-                        </CardBody>
-                    </Card>
-                    </div>
-                </Col>    
-                <Col md={3}>
-                    <div> 
-                    <Card>
-                        <CardImg top width="100%" src={Image3} alt="Card image cap" />
-                        <CardBody>
-                        <button className="functionButtons" onClick={onViewJob}>VIEW NEW JOB POSTINGS</button>
-                        </CardBody>
-                    </Card>
-                    
-                    </div>
-                </Col>  
+                <Row>
+                    <Col md={5}>
+                        <div className="parentdiv">
+                            <div className="childdiv">
+                                <ModalExample buttonLabel="ADD NEW RESOURCES"></ModalExample>
+                            </div>    
+                        </div>
+                        <div className="parentdiv">
+                            <div className="childdiv">
+                                <button className="functionButtons" onClick={onViewProfile}>VIEW STUDENT PROFILE</button>
+                            </div>
+                        </div>
+                        <div className="parentdiv">
+                            <div className="childdiv"> 
+                                <AddNewJobModal buttonLabel="ADD NEW JOB POSTINGS"><i class="fas fa-box-open"></i></AddNewJobModal>
+                            </div> 
+                        </div>
+                    </Col>
+                    <Col md={5}>    
+                        <div className="parentdiv">
+                            <div className="childdiv"> 
+                                <AddNewTestModal buttonLabel="ADD NEW TEST"/>
+                            </div>
+                        </div>
+                        <div className="parentdiv">
+                            <div className="childdiv"> 
+                                <button className="functionButtons" onClick={onViewJob}>VIEW NEW JOB/POSTINGS</button>
+                            </div>
+                        </div>
+                    </Col>
                 
-                  
-                 {/* <ModalExample buttonLabel="ADD NEW RESOURCES"></ModalExample>
-                        <hr/>
-                        <AddNewJobModal buttonLabel="ADD NEW JOB POSTINGS"><i class="fas fa-box-open"></i></AddNewJobModal>
-                        <hr/>
-                        <button className="functionButtons" onClick={onViewJob}>VIEW NEW JOB POSTINGS</button>
-                        <hr/>
-                        <button className="functionButtons" onClick={onViewProfile}>VIEW STUDENT PROFILE</button> 
-                        <hr/>
-                        <AddNewTestModal buttonLabel="ADD NEW TEST"/>
-                        <hr/> */}
-                    
-                <Col md={3}>
-                    <div className="schedule" align="center">
-                        SCHEDULES <br/>
-                    </div>
-                    <div className="notices" align="center">
-                        NOTICE 1 <br/>
-                        NOTICE 2 <br/>
-                        NOTICE 3 <br/>
-                        NOTICE 1 <br/>
-                        NOTICE 2 <br/>
-                        NOTICE 3 <br/>
-                    </div>
-                </Col>
-            </Row>
+                    <Col md={2}>
+                        <div className="schedule" align="center">
+                            SCHEDULES <br/>
+                        </div>
+                        <div className="notices" align="center">
+                            NOTICE 1 <br/>
+                            NOTICE 2 <br/>
+                            NOTICE 3 <br/>
+                            NOTICE 1 <br/>
+                            NOTICE 2 <br/>
+                            NOTICE 3 <br/>
+                        </div>
+                    </Col>
+                </Row>
             </div>
             <Footer/>
-                
         </div>
     );
 }
