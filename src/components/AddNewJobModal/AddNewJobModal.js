480a7667e4ab27373eebbc14c9ea5be88cdd39f7
Neha Goel <nehagoel66@gmail.com>
2020-11-10 15:16:51 +0530

    styling completed:

--- src/components/AddNewJobModal/AddNewJobModal.js
+++ src/components/AddNewJobModal/AddNewJobModal.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { Label, Input, Button } from 'reactstrap';
+import './AddNewJobModal.css';
+import { Label, Input, Button, FormGroup, Card, CardBody, Form } from 'reactstrap';
 
 const AddNewJob = (props) => {
 
@@ -8,19 +9,50 @@ const AddNewJob = (props) => {
     const [packages, setPackages] = useState("");
 
     return (
-        <div>
-            <div align="center">ADD INFORMATION ABOUT NEW JOB POSTING</div>
-            <hr/>
-            <Label for="company">Company</Label>
-            <Input type="text" name="company" id="company" value={company} placeholder="Enter Company" onChange={e => setCompany(e.target.value)}/>
-            <br/>
-            <Label for="jobprofile">Job Profile</Label>
-            <Input type="text" name="jobprofile" id="jobprofile" value={jobprofile} placeholder="Enter Job Profile" onChange={e => setJobProfile(e.target.value)}/>
-            <br/>
-            <Label for="package">Package</Label>
-            <Input type="text" name="packages" id="packages" value={packages} placeholder="Enter Package of company" onChange={e => setPackages(e.target.value)}/>
-            <br/>
-            <center><Button>UPLOAD</Button> </center>           
+        <div className="containerj">
+            <Card className="job-upload-form">
+                <CardBody className="upload-form">
+                    <Form onSubmit="" autoComplete="off">
+                        <FormGroup align="center">
+                            <Label>ADD INFORMATION ABOUT NEW JOB/POSTINGS</Label>
+                        </FormGroup>
+                        <hr/>
+                        <FormGroup>
+                            <Label for="company">Company</Label>
+                            <Input 
+                                type="text" 
+                                name="company" 
+                                id="company" 
+                                value={company} 
+                                placeholder="Enter Company" 
+                                onChange={e => setCompany(e.target.value)}/>
+                        </FormGroup>    
+                        <FormGroup>
+                            <Label for="jobprofile">Job Profile</Label>
+                            <Input 
+                                type="text" 
+                                name="jobprofile" 
+                                id="jobprofile" 
+                                value={jobprofile} 
+                                placeholder="Enter Job Profile" 
+                                onChange={e => setJobProfile(e.target.value)}/>
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="package">Package</Label>
+                            <Input 
+                                type="text" 
+                                name="packages" 
+                                id="packages" 
+                                value={packages} 
+                                placeholder="Enter Package of company" 
+                                onChange={e => setPackages(e.target.value)}/>
+                        </FormGroup>
+                        <FormGroup align="center">
+                            <Button>UPLOAD</Button>
+                        </FormGroup>
+                    </Form>
+                </CardBody>    
+            </Card>         
         </div>
     );
 }
--- src/components/FileUploadTest/FileUploadTest.js
+++ src/components/FileUploadTest/FileUploadTest.js
@@ -1,14 +1,14 @@
 import React, { useEffect, useState } from 'react';
-import './FileUploadTest.css'
-import { Progress,Form, Col, Row, Label, FormGroup } from 'reactstrap';
+import './FileUploadTest.css';
+import { Progress, Button, Form, FormGroup, Label, Input, Card, CardBody } from 'reactstrap';
 
 const FileUploadTest = () => {
 
   const [totalData, setTotalData] = useState(0);
-  const [loadedData, setLoadedData] = useState(0)
-  const [loading, setLoading] = useState(false)
-  const [loaded, setLoaded] = useState(false)
-  const [category, setCategory] = useState("")
+  const [loadedData, setLoadedData] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const [loaded, setLoaded] = useState(false);
+  const [category, setCategory] = useState("");
   const [subject, setSubject] = useState("");
   const [subjectList, setSubjectList] = useState([]);
 
@@ -75,94 +75,46 @@ const FileUploadTest = () => {
   }
 
 
+  return (
 
-  // <Form>
-  //       <FormGroup>
-  //         <label htmlFor="exampleFormControlInput1">Email address</label>
-  //         <Input
-  //           id="exampleFormControlInput1"
-  //           placeholder="name@example.com"
-  //           type="email"
-  //         ></Input>
-  //       </FormGroup>
-  //       <FormGroup>
-  //         <label htmlFor="exampleFormControlSelect1">Example select</label>
-  //         <Input id="exampleFormControlSelect1" type="select">
-  //           <option>1</option>
-  //           <option>2</option>
-  //           <option>3</option>
-  //           <option>4</option>
-  //           <option>5</option>
-  //         </Input>
-  //       </FormGroup>
-  //       <FormGroup>
-  //         <label htmlFor="exampleFormControlSelect2">
-  //           Example multiple select
-  //         </label>
-  //         <Input
-  //           id="exampleFormControlSelect2"
-  //           multiple="multiple"
-  //           type="select"
-  //         >
-  //           <option>1</option>
-  //           <option>2</option>
-  //           <option>3</option>
-  //           <option>4</option>
-  //           <option>5</option>
-  //         </Input>
-  //       </FormGroup>
-  //       <FormGroup>
-  //         <label htmlFor="exampleFormControlTextarea1">Example textarea</label>
-  //         <Input id="exampleFormControlTextarea1" rows="3" type="textarea"></Input>
-  //       </FormGroup>
-  //     </Form>
-
-    return (
-      <div className="containerj">
-        <Row>
-        <Form onSubmit={uploadFile}>
-          <FormGroup>
-          <Col md={3}>
-            <label>Upload a file</label>
-            <input id="file-field" type="file" />
-          </Col>
-          </FormGroup>
-          <FormGroup>
-          <Col md={3}>
-            <Label>Category</Label>
-
-            <select value={category} className="select" onChange={(e) => setCategory(e.target.value)} required>
-              <option value="">--Please choose an option--</option>
-              <option value="assignment">Assignment</option>
-              <option value="testpaper">Test Paper</option>
-              <option value="notes">Notes</option>
-           </select>
-           </Col>
-           </FormGroup>
-           <FormGroup>
-           <Col md={3}>
-           <Label>Subject</Label>
-           <select value={subject} className="select" onChange={(e) => setSubject(e.target.value)} >
-              <option value="">--Please choose an option--</option>
-              {
-                subjectList.map((sub) => <option value={sub._id} key={sub._id}>{sub.title}</option>)
-              }
-           </select>
-           
-           </Col>
-           </FormGroup>
-           <FormGroup>
-           <Col md={3}>
-
-            <input type="submit" />
-            </Col>
+    <div className="containerj">
+      <Card className="file-upload-form">
+        <CardBody className="upload-form">
+          <Form onSubmit={uploadFile} autoComplete="off">
+            <FormGroup align="center">
+              <Label for="upload">UPLOAD A FILE</Label>
+              <hr/>
+              <Input name="file" id="file-field" type="file" />
             </FormGroup>
-        </Form>
-        </Row>
-        {loading &&  <Progress animated max={totalData} value={loadedData} />}
-        {loaded && <h1>File Successfully uploaded! </h1>}
+            <FormGroup>
+              <Label for="category">CATEGORY</Label>
+              <Input type="select" value={category} className="select" onChange={(e) => setCategory(e.target.value)} required>
+                <option value="">--Please choose an option--</option>
+                <option value="assignment">Assignment</option>
+                <option value="testpaper">Test Paper</option>
+                <option value="notes">Notes</option>
+              </Input>
+            </FormGroup>
+            <FormGroup>
+              <Label>SUBJECT</Label>
+              <Input type="select" value={subject} className="select" onChange={(e) => setSubject(e.target.value)} >
+                <option value="">--Please choose an option--</option>
+                {
+                  subjectList.map((sub) => <option value={sub._id} key={sub._id}>{sub.title}</option>)
+                }
+              </Input>
+            </FormGroup>
+            <FormGroup align="center">
+              <Button type="submit">SUBMIT</Button>
+            </FormGroup>
+          </Form>
+          {loading &&  <Progress animated max={totalData} value={loadedData} />}
+          {loaded && <h1>File Successfully uploaded! </h1>}
+        </CardBody>
+      </Card>
     </div>
-    )
+  );
 }
 
 export default FileUploadTest;
+
