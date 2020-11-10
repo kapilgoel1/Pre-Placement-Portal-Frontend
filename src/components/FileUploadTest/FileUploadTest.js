8076f333f25cb0a3922976780e5f40b910f8519c
Neha Goel <nehagoel66@gmail.com>
2020-11-10 09:30:00 +0530

    working

--- src/components/FileUploadTest/FileUploadTest.js
+++ src/components/FileUploadTest/FileUploadTest.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { Progress } from 'reactstrap';
+import './FileUploadTest.css'
+import { Progress,Form, Col, Row, Label, FormGroup } from 'reactstrap';
 
 const FileUploadTest = () => {
 
@@ -73,29 +74,95 @@ const FileUploadTest = () => {
     xhrObj.send(formData);
   }
 
+
+
+  // <Form>
+  //       <FormGroup>
+  //         <label htmlFor="exampleFormControlInput1">Email address</label>
+  //         <Input
+  //           id="exampleFormControlInput1"
+  //           placeholder="name@example.com"
+  //           type="email"
+  //         ></Input>
+  //       </FormGroup>
+  //       <FormGroup>
+  //         <label htmlFor="exampleFormControlSelect1">Example select</label>
+  //         <Input id="exampleFormControlSelect1" type="select">
+  //           <option>1</option>
+  //           <option>2</option>
+  //           <option>3</option>
+  //           <option>4</option>
+  //           <option>5</option>
+  //         </Input>
+  //       </FormGroup>
+  //       <FormGroup>
+  //         <label htmlFor="exampleFormControlSelect2">
+  //           Example multiple select
+  //         </label>
+  //         <Input
+  //           id="exampleFormControlSelect2"
+  //           multiple="multiple"
+  //           type="select"
+  //         >
+  //           <option>1</option>
+  //           <option>2</option>
+  //           <option>3</option>
+  //           <option>4</option>
+  //           <option>5</option>
+  //         </Input>
+  //       </FormGroup>
+  //       <FormGroup>
+  //         <label htmlFor="exampleFormControlTextarea1">Example textarea</label>
+  //         <Input id="exampleFormControlTextarea1" rows="3" type="textarea"></Input>
+  //       </FormGroup>
+  //     </Form>
+
     return (
-      <div>
-        <form onSubmit={uploadFile}>
-          <input id="file-field" type="file" />
-            <select value={category} onChange={(e) => setCategory(e.target.value)} required>
+      <div className="containerj">
+        <Row>
+        <Form onSubmit={uploadFile}>
+          <FormGroup>
+          <Col md={3}>
+            <label>Upload a file</label>
+            <input id="file-field" type="file" />
+          </Col>
+          </FormGroup>
+          <FormGroup>
+          <Col md={3}>
+            <Label>Category</Label>
+
+            <select value={category} className="select" onChange={(e) => setCategory(e.target.value)} required>
               <option value="">--Please choose an option--</option>
               <option value="assignment">Assignment</option>
               <option value="testpaper">Test Paper</option>
               <option value="notes">Notes</option>
            </select>
-           <select value={subject} onChange={(e) => setSubject(e.target.value)} >
-              <option value="">--Please choose an options--</option>
+           </Col>
+           </FormGroup>
+           <FormGroup>
+           <Col md={3}>
+           <Label>Subject</Label>
+           <select value={subject} className="select" onChange={(e) => setSubject(e.target.value)} >
+              <option value="">--Please choose an option--</option>
               {
                 subjectList.map((sub) => <option value={sub._id} key={sub._id}>{sub.title}</option>)
               }
            </select>
+           
+           </Col>
+           </FormGroup>
+           <FormGroup>
+           <Col md={3}>
 
             <input type="submit" />
-        </form>
+            </Col>
+            </FormGroup>
+        </Form>
+        </Row>
         {loading &&  <Progress animated max={totalData} value={loadedData} />}
         {loaded && <h1>File Successfully uploaded! </h1>}
     </div>
     )
 }
 
-export default FileUploadTest
+export default FileUploadTest;
