947e95b173c86b06ac921dcc69c74718dd5d095b
kapil-wq <59357022+kapil-wq@users.noreply.github.com>
2020-11-17 23:29:44 +0530

    edited jobprofile input

--- src/components/AddNewJobModal/AddNewJobModal.js
+++ src/components/AddNewJobModal/AddNewJobModal.js
@@ -56,6 +56,7 @@ const AddNewJob = (props) => {
                             <Label for="jobprofile">Job Profile</Label>
                             <Input 
                                 type="textarea" 
+                                className="jobprofile"
                                 name="jobprofile" 
                                 id="jobprofile" 
                                 value={jobprofile} 
