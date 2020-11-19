56a2d2f0ecd29d480a909399b219add4532398e1
Neha Goel <nehagoel66@gmail.com>
2020-11-19 19:36:12 +0530

    working

--- src/containers/StudentEditProfile/StudentEditProfile.js
+++ src/containers/StudentEditProfile/StudentEditProfile.js
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from 'react';
-import { useHistory } from 'react-router-dom'
 import './StudentEditProfile.css';
-import { Label, Button, Row, Col, Input } from 'reactstrap';
-import IMAGE from '../../assets/Image6.jpg';
+import { Input, Label, Button, Form, Card, CardBody,FormGroup } from 'reactstrap';
+import StudentNavBar from '../../components/StudentNavbar/StudentNavBar';
 
 const StudentEditProfile = (props) => {
   const [firstName, setFirstName] = useState('');
@@ -15,8 +14,6 @@ const StudentEditProfile = (props) => {
   const [motherName, setMotherName] = useState('');
   const [fatherName, setFatherName] = useState('');
   const [password, setPassword] = useState('');
-
-  const history = useHistory();
 
   useEffect(() => {
     fetch('http://localhost:4000/user/details', {
@@ -72,7 +69,7 @@ const StudentEditProfile = (props) => {
     })
       .then((response) => response.json())
       .then((result) => {
-        history.push('/studentdashboard');
+        props.history.push('/studentdashboard');
         console.log(result);
       })
       .catch((err) => {
@@ -81,117 +78,132 @@ const StudentEditProfile = (props) => {
   };
 
   const onCancelHandler = () => {
-    history.push('/studentdashboard');
+    props.history.push('/studentdashboard');
   }
 
   return (
-    <div className="jcontainer">
-      <Row>
-        <Col lg={6}>
-          <div className="picdiv">
-            <img src={IMAGE} alt={IMAGE} className="pic"/>
-          </div>
-        </Col>
-        <Col lg={5}>
-          <h1>Edit Profile</h1>
-          <Label for="firstName">First Name </Label>
-          <Input
-            type="text"
-            name="firstName"
-            value={firstName}
-            placeholder=""
-            onChange={(e) => setFirstName(e.target.value)}
-          />
-          <Label for="lastName">Last Name </Label>
-          <Input
-            type="text"
-            name="lastName"
-            value={lastName}
-            placeholder=""
-            onChange={(e) => setLastName(e.target.value)}
-          />
-          <Label for="course">Course </Label>
-          <Input
-            type="text"
-            name="course"
-            value={course}
-            placeholder=""
-            onChange={(e) => setCourse(e.target.value)}
-          />
-          <Label for="semester">Semester </Label>
-          <Input
-            type="text"
-            name="semester"
-            value={semester}
-            placeholder=""
-            onChange={(e) => setSemester(e.target.value)}
-          />
-          <Label for="phone">Mobile No. </Label>
-          <Input
-            type="text"
-            name="phone"
-            value={phone}
-            placeholder=""
-            onChange={(e) => setPhone(e.target.value)}
-          />
-          <Label for="dob">DOB </Label>
-          <Input
-            type="date"
-            name="dob"
-            value={dob}
-            placeholder=""
-            onChange={(e) => setdob(e.target.value)}
-          />
-          <Label for="address">Address </Label>
-          <Input
-            type="textarea" 
-            className="address"
-            name="address"
-            value={address}
-            placeholder=""
-            onChange={(e) => setAddress(e.target.value)}
-          />
-          <Label for="mothername">Mother's Name </Label>
-          <Input
-            type="text"
-            name="mothername"
-            value={motherName}
-            placeholder=""
-            onChange={(e) => setMotherName(e.target.value)}
-          />
-          <Label for="fathername">Father's Name </Label>
-          <Input
-            type="text"
-            name="fathername"
-            value={fatherName}
-            placeholder=""
-            onChange={(e) => setFatherName(e.target.value)}
-          />
-          <Label for="newpassword">New Password</Label>
-          <Input
-            type="password"
-            name="newpassword"
-            value={password}
-            placeholder=""
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <div className="button1">
-          <Row>
-            <Col sm={4}>
-              <Button color="success" onClick={onUpdateHandler}>
-                Update Profile
-              </Button>
-            </Col>
-            <Col sm={4}>
-              <Button color="danger" onClick={onCancelHandler}>
-                Cancel
-              </Button>
-            </Col>
-          </Row>  
-          </div>
-        </Col>
-        <Col lg={1}></Col>
-      </Row>
+    <div className="scontainer">
+        <StudentNavBar/>
+        <div className="containerj">
+            <Card className="student-edit-profile-form">
+                <CardBody className="upload-form">
+                  <Form onSubmit="" autoComplete="off">
+                    <FormGroup align="center">
+                      <Label>EDIT PROFILE</Label>
+                    </FormGroup>
+                    <hr/>
+                    <FormGroup>
+                      <Label for="firstName">First Name </Label>
+                      <Input
+                        type="text"
+                        name="firstName"
+                        value={firstName}
+                        placeholder=""
+                        onChange={(e) => setFirstName(e.target.value)}
+                      />
+                    </FormGroup>
+                    <FormGroup>
+                      <Label for="lastName">Last Name </Label>
+                      <Input
+                        type="text"
+                        name="lastName"
+                        value={lastName}
+                        placeholder=""
+                        onChange={(e) => setLastName(e.target.value)}
+                      />
+                    </FormGroup>
+                    <FormGroup>
+                      <Label for="course">Course </Label>
+                      <Input
+                        type="text"
+                        name="course"
+                        value={course}
+                        placeholder=""
+                        onChange={(e) => setCourse(e.target.value)}
+                      />
+                    </FormGroup>
+                    <FormGroup>
+                      <Label for="semester">Semester </Label>
+                      <Input
+                        type="text"
+                        name="semester"
+                        value={semester}
+                        placeholder=""
+                        onChange={(e) => setSemester(e.target.value)}
+                      />
+                    </FormGroup> 
+                    <FormGroup>
+                      <Label for="phone">Mobile No. </Label>
+                      <Input
+                        type="text"
+                        name="phone"
+                        value={phone}
+                        placeholder=""
+                        onChange={(e) => setPhone(e.target.value)}
+                      />
+                    </FormGroup>
+                    <FormGroup>
+                      <Label for="dob">DOB </Label>
+                      <Input
+                        type="date"
+                        name="dob"
+                        value={dob}
+                        placeholder=""
+                        onChange={(e) => setdob(e.target.value)}
+                      />
+                    </FormGroup>
+                    <FormGroup>
+                      <Label for="address">Address </Label>
+                      <Input
+                        type="textarea"
+                        name="address"
+                        value={address}
+                        placeholder=""
+                        onChange={(e) => setAddress(e.target.value)}
+                      />
+                    </FormGroup>
+                    <FormGroup>
+                      <Label for="mothername">Mother's Name </Label>
+                      <Input
+                        type="text"
+                        name="mothername"
+                        value={motherName}
+                        placeholder=""
+                        onChange={(e) => setMotherName(e.target.value)}
+                      />
+                    </FormGroup>
+                    <FormGroup>
+                      <Label for="fathername">Father's Name </Label>
+                      <Input
+                        type="text"
+                        name="fathername"
+                        value={fatherName}
+                        placeholder=""
+                        onChange={(e) => setFatherName(e.target.value)}
+                      />
+                    </FormGroup>
+                    <FormGroup>
+                      <Label for="newpassword">New Password</Label>
+                      <Input
+                        type="password"
+                        name="newpassword"
+                        value={password}
+                        placeholder=""
+                        onChange={(e) => setPassword(e.target.value)}
+                      />
+                    </FormGroup>
+                    <FormGroup align='center'>
+                      <Button color="success" onClick={onUpdateHandler}>
+                        Update Profile
+                      </Button>
+                      <Button color="danger" onClick={onCancelHandler}>
+                        Cancel
+                      </Button>
+                    </FormGroup>
+                  </Form>
+                </CardBody>    
+              </Card>  
+            </div>  
     </div>
   );
 };
