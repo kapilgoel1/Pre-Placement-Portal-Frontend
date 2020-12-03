3ce480d24e1c856e8153edfeba73a02456ef7b89
Neha Goel <nehagoel66@gmail.com>
2020-12-03 17:40:05 +0530

    slight chng

--- src/containers/Home/home.js
+++ src/containers/Home/home.js
@@ -45,10 +45,10 @@ function Home() {
         <div className="about-portal">
           <h1 className="px-2 my-3">JIMS MCA PrePlacement Portal</h1>
           <p className="px-2 my-2 lead">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
-            dolores, odio ut totam repellendus earum doloribus recusandae
-            voluptatem vel aut alias iste? Est, ab pariatur officia optio eos
-            iste cumque.
+            JIMS welcomes all eligible students to the placement season. We
+            strive to extend all possible support to provide the right career
+            opportunities to our students to fruitfully pursue their career
+            interests.
           </p>
           <Button className="py-2 my-4" color="color4" size="lg">
             Sign Up
